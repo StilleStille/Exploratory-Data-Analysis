aa11fa80d1fabea8d02319a97dc62bde8381295f
StilleStille <kudrina.m@outlook.com>
2014-10-08 21:10:13 +0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -9,6 +9,5 @@ data$Datetime <- strptime(paste(data$Date,data$Time),
 format="%Y-%m-%d %H:%M:%S")
 hist(data$Global_active_power, main="Global Active Power",
 xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-## Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
